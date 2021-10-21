2c0eb8c6a7681f908effc5ec79a51f93b9ab419e
Stuart-Yee <ki6fzb@gmail.com>
2021-10-20 20:14:17 -0700

    adding display method

---
+++ Display/display.js
@@ -0,0 +1,64 @@
+class SLL {
+    constructor(){
+        this.head = null;
+    }
+
+    addFront(value) {
+        var node = node(value);
+        if (this.head == null){
+            this.head = node;
+        } else {
+            node.next = this.head;
+            this.head = node;
+        }
+        return this.head;
+    }
+
+    removeFront(){
+        if(this.head = null){
+            return null;
+        }else if(this.head.next == null){
+            this.head = null;
+            return null;
+        } else {
+            var runner = this.head.next;
+            this.head = null;
+            this.head = runner;
+            return this.head;
+        }
+    }
+
+    front(){
+        if(this.head = null){
+            return null;
+        } else {
+            return this.head.data;
+        }
+    }
+
+    display(){
+        var chain = "";
+        if(this.head == null){
+            return null;
+        }
+        pointer = this.head;
+        while(true){
+            chain += pointer.data;
+            if(pointer.next == null){
+                console.log(chain);
+                return chain;
+            }
+            chain += ", ";
+            runner = pointer.next;
+            pointer = runner;
+        }
+    }
+}
+
+class Node{
+    constructor(data){
+        this.data=data;
+        this.next=null;
+    }
+}
+
